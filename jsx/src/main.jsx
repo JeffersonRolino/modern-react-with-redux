8b7f52358f6f2805fd704f3a87da644345017678
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-05 15:12:27 -0300

    JSX Project - Lesson 10: Showing Basic Content

--- jsx/src/main.jsx
+++ jsx/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// 1) Import the React and ReactDom libraries
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 2) Get a reference to the div with ID root
+const rootElement = document.getElementById('root');
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(rootElement);
+
+// 4) Create a component
+function App() {
+  return <h1>Hi there!</h1>
+}
+
+// 5) Show the component on the screen
+root.render(<App/>);
