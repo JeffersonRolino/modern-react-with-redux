c525845684b658b0628472df3cf266e964cdb0d7
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-05 22:19:15 -0300

    JSX Project - Lesson 19: Applying Styling in JSX

--- jsx/src/main.jsx
+++ jsx/src/main.jsx
@@ -12,7 +12,20 @@ const root = ReactDOM.createRoot(rootElement);
 // 4) Create a component
 function App() {
   
-  return <textarea autoFocus={true}/>;
+
+  return <div style={{display: "flex", flexDirection: "column", gap:"20px"}}>
+    {/* In JSX, the 'class' attribute is written as 'className' because 'class' is a reserve word in JavaScript */}
+    <h1 className="main-title">JSX Rules</h1>
+
+    {/* All props names follow camelCase */}
+    <textarea name="text" autoFocus={true}></textarea>
+
+    {/* In JSX Attributes meant to be numbers should be provide as numbers with curly braces */}
+    <input type="number" maxLength={5} />
+
+    {/* In JSX boolean 'true' can be written with just property name. 'False' should be written with curly braces */}
+    <input spellCheck/>
+  </div>
 }
 
 // 5) Show the component on the screen
