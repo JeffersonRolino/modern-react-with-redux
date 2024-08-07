86244cf27c97a5fbb96bbaecb9880c99f4806bd8
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-07 12:38:17 -0300

    PDAs Project - Lesson 28: Adding Props

--- pdas/src/components/ProfileCard.jsx
+++ pdas/src/components/ProfileCard.jsx
@@ -1,9 +1,10 @@
-function ProfileCard() {
+function ProfileCard(props) {
   return (
     <div>
-      Profile Card
+      <div>Title is {props.title}</div>
+      <div>Handle is {props.handle}</div>
     </div>
-  )
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
