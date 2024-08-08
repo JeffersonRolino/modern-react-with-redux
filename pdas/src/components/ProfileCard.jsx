2f13ac68a8655259e9903fb170d1a10500499289
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-08 17:24:15 -0300

    PDAs Project - Lesson 38: A Big Pile of HTML

--- pdas/src/components/ProfileCard.jsx
+++ pdas/src/components/ProfileCard.jsx
@@ -1,9 +1,17 @@
 function ProfileCard({ title, handle, image }) {
   return (
-    <div>
-      <img src={image} alt="Personal Digital Assistant logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="Personal Digital Assistant logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content"></div>
+        <div className="title is-4">{title}</div>
+        <div className="subtitle is-6">{handle}</div>
+      </div>
     </div>
   );
 }
