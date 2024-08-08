dc8065c18550a7bf8b1ea5ad094ea9de0a6282dd
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-07 21:20:49 -0300

    PDAs Project - Lesson 34: Including Images

--- pdas/src/App.jsx
+++ pdas/src/App.jsx
@@ -1,9 +1,20 @@
 import ProfileCard from "./components/ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
     <div>
       <h1>Personal Digital Assistants</h1>
+
+      <img src={AlexaImage} />
+      <img src={CortanaImage} />
+      <img src={SiriImage} />
+
       <ProfileCard title="Alexa" handle="@alexa99" />
       <ProfileCard title="Cortana" handle="@cortana32" />
       <ProfileCard title="Siri" handle="@siri01" />
