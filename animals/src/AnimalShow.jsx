f44bb6131bd2cea5ccf971356814765308ed52ea
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-11 17:20:32 -0300

    Animals Project - Lesson 54: Loading and Showing SVGs

--- animals/src/AnimalShow.jsx
+++ animals/src/AnimalShow.jsx
@@ -1,7 +1,19 @@
+import bird from './images/bird.svg';
+import cat from './images/cat.svg';
+import cow from './images/cow.svg';
+import dog from './images/dog.svg';
+import gator from './images/gator.svg';
+import heart from './images/heart.svg';
+import horse from './images/horse.svg';
+
+const svgMap = {
+  bird, cat, cow, dog, gator, heart, horse
+}
+
 function AnimalShow({type}) {
   return (
     <div>
-      {type}
+      <img src={svgMap[type]} alt={type} />
     </div>
   )
 }
