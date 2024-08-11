a9575b7035e3354935ef2a59141104d5e02f5898
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-11 17:26:49 -0300

    Animals Project - Lesson 55: Increasing Image Size

--- animals/src/AnimalShow.jsx
+++ animals/src/AnimalShow.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import bird from './images/bird.svg';
 import cat from './images/cat.svg';
 import cow from './images/cow.svg';
@@ -7,13 +8,20 @@ import heart from './images/heart.svg';
 import horse from './images/horse.svg';
 
 const svgMap = {
-  bird, cat, cow, dog, gator, heart, horse
+  bird, cat, cow, dog, gator, horse
 }
 
 function AnimalShow({type}) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  }
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <img src={svgMap[type]} alt={type} />
+      <img src={heart} alt="heart" style={{ width: 10 + 10 * clicks}}/>
     </div>
   )
 }
