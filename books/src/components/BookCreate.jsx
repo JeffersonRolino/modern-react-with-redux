057739eb72adf37822868995da13240327f29334
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-15 11:25:42 -0300

    Books Project - Lesson 88: Receiving New Titles

--- books/src/components/BookCreate.jsx
+++ books/src/components/BookCreate.jsx
@@ -1,7 +1,25 @@
-function BookCreate() {
+import { useState } from "react"
+
+function BookCreate({onCreate}) {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('');
+  }
+
   return (
     <div>
-      BookCreate
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} />
+        <button>Create</button>
+      </form>
     </div>
   )
 }
