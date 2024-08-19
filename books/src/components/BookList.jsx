e28b5fcec737f58b5345d73ff7c7974aecdf0b11
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-19 10:26:10 -0300

    Books Project - Lesson 131: Context in Action

--- books/src/components/BookList.jsx
+++ books/src/components/BookList.jsx
@@ -1,12 +1,17 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 function BookList({books, onDelete, onEdit}) {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />;
   })
 
   return (
     <div className="book-list">
+      {value}
       {renderedBooks}
     </div>
   )
--- books/src/main.jsx
+++ books/src/main.jsx
@@ -1,10 +1,13 @@
 import './index.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import BooksContext from "./context/books.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BooksContext.Provider value={10}>
+      <App />
+    </BooksContext.Provider>
   </StrictMode>,
 )
