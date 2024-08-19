a3e50aa7b446bf819cbae9e855ca3b908ff3426a
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-19 10:53:54 -0300

    Books Project - Lesson 133: More on Changing Context

--- books/src/main.jsx
+++ books/src/main.jsx
@@ -2,12 +2,12 @@ import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import BooksContext from "./context/books.jsx";
+import { Provider } from "./context/books.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BooksContext.Provider value={10}>
+    <Provider>
       <App />
-    </BooksContext.Provider>
+    </Provider>
   </StrictMode>,
 )
