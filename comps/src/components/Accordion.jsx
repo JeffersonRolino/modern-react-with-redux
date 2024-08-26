b1300b06c73d55e7f0eea5873dd2c64f781d1f24
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-26 07:51:03 -0300

    Comps Project - Lesson 182: Conditional Rendering

--- comps/src/components/Accordion.jsx
+++ comps/src/components/Accordion.jsx
@@ -6,16 +6,10 @@ function Accordion({items}) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    if(isExpanded){
-      console.log(index, 'expanded');
-    }
-    else {
-      console.log(index, 'collapsed');
-    }
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
