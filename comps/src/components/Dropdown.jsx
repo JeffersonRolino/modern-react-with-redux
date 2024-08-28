bbbf7e00e3dc5055f542478d3f1bd9b60d511453
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-28 11:00:02 -0300

    Comps Project - Lesson 198: Reminder on Event Handlers in Maps

--- comps/src/components/Dropdown.jsx
+++ comps/src/components/Dropdown.jsx
@@ -5,11 +5,19 @@ function Dropdown({options}) {
 
   const handleClick = () => {
     setIsOpen(!isOpen);
-  }  
+  }
+
+  const handleOptionClick = (option) => {
+    // Close Dropdown
+    setIsOpen(false);
+
+    // Selected Option
+    console.log(option);
+  }
 
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value}>
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
         {option.value}
       </div>
     )
