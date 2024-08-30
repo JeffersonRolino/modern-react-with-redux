468ef3ab0b696927b073f05a3e107b6594fa4243
jeffersonrolino <jefferson.rolino@gmail.com>
2024-08-30 09:36:57 -0300

    Comps project - Lesson 212: Reminder on the useEffect Function

--- comps/src/App.jsx
+++ comps/src/App.jsx
@@ -1,20 +1,24 @@
-import Dropdown from "./components/Dropdown"
-import { useState } from "react"
+import Dropdown from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
   const [selection, setSelection] = useState(null);
 
   const handleSelect = (option) => {
     setSelection(option);
-  }
+  };
 
   const options = [
-    {label: 'Red', value: 'red'},
-    {label: 'Green', value: 'green'},
-    {label: 'Blue', value: 'blue'},
-  ]
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
 
-  return <Dropdown options={options} value={selection} onChange={handleSelect}/>
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
 }
 
-export default App
+export default App;
