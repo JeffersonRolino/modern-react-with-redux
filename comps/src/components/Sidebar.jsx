b6861e4579824ba233ab677f5544a99ff9efc44d
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-05 15:51:42 -0300

    Comps Project - Lesson 233: Highlighting the Active Link

--- comps/src/components/Sidebar.jsx
+++ comps/src/components/Sidebar.jsx
@@ -8,10 +8,15 @@ function Sidebar() {
   ];
 
   return (
-    <div className="sticky top-0 flex flex-col">
+    <div className="sticky top-0 flex flex-col items-start">
       {links.map((link) => {
         return (
-          <Link key={link.label} to={link.path}>
+          <Link
+            key={link.label}
+            to={link.path}
+            className="mb-3"
+            activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+          >
             {link.label}
           </Link>
         );
