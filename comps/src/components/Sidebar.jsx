a311da5efdb16ef8139f55c5a9e619c13a94b878
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-05 15:42:56 -0300

    Comps Project - Lesson 232: Adding Sidebar Component

---
+++ comps/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import Link from "./Link";
+
+function Sidebar() {
+  const links = [
+    { label: "Dropdown", path: "/" },
+    { label: "Accordion", path: "/accordion" },
+    { label: "Buttons", path: "/buttons" },
+  ];
+
+  return (
+    <div className="sticky top-0 flex flex-col">
+      {links.map((link) => {
+        return (
+          <Link key={link.label} to={link.path}>
+            {link.label}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Sidebar;
--- comps/src/pages/ButtonPage.jsx
+++ comps/src/pages/ButtonPage.jsx
@@ -1,30 +1,41 @@
 import Button from "../components/Button";
-import {GoBell, GoCheckCircleFill , GoCodeOfConduct  } from "react-icons/go";
+import { GoBell, GoCheckCircleFill, GoCodeOfConduct } from "react-icons/go";
 
 function ButtonPage() {
-  const handleClick = () => {
-    console.log("clicked!!!");
-  }
+  const handleClick = () => {};
 
   return (
     <div>
       <div>
-        <Button primary outline rounded onClick={handleClick}> <GoBell/>Click Me!</Button>
+        <Button primary outline rounded onClick={handleClick}>
+          {" "}
+          <GoBell />
+          Click Me!
+        </Button>
       </div>
       <div>
-        <Button secondary className="mb-5 mt-5"><GoCheckCircleFill /> Buy Now</Button>
+        <Button secondary className="mb-5 mt-5">
+          <GoCheckCircleFill /> Buy Now
+        </Button>
       </div>
       <div>
-        <Button success outline> <GoCodeOfConduct /> See Deal!</Button>
+        <Button success outline>
+          {" "}
+          <GoCodeOfConduct /> See Deal!
+        </Button>
       </div>
       <div>
-        <Button warning rounded>Hide Ads</Button>
+        <Button warning rounded>
+          Hide Ads
+        </Button>
       </div>
       <div>
-        <Button danger rounded>Close</Button>
+        <Button danger rounded>
+          Close
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
 export default ButtonPage;
